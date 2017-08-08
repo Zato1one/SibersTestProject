affa10128b2a1b2f722365be2280cf2f4e29730a
stazher <stazher@ASP-086>
2017-08-08 15:56:40 +0700

    start BL, complite User/RoleManager

---
+++ SibersTestProject.Logic.BL/Identity/ProjectRoleManager.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity;
+using SibersTestProject.Data.DAL.Identity.Entities;
+using System;
+
+namespace SibersTestProject.Logic.BL.Identity
+{
+    public class ProjectRoleManager : RoleManager<ProjectRole, Guid>
+    {
+        public ProjectRoleManager(IRoleStore<ProjectRole, Guid> roleStore)
+            : base(roleStore) {
+        }
+    }
+}
---
+++ SibersTestProject.Logic.BL/Identity/ProjectUserManager.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Identity;
+using SibersTestProject.Data.DAL.Identity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SibersTestProject.Logic.BL.Identity
+{
+    public class ProjectUserManager : UserManager<ProjectUser, Guid>
+    {
+        public ProjectUserManager(IUserStore<ProjectUser, Guid> store)
+            : base(store)
+        {
+
+            // Configure validation logic for usernames
+            this.UserValidator = new UserValidator<ProjectUser, Guid>(this)
+            {
+                AllowOnlyAlphanumericUserNames = true,
+                RequireUniqueEmail = false
+            };
+
+            // Configure validation logic for passwords
+            this.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
+            };
+
+        }
+    }
+}
